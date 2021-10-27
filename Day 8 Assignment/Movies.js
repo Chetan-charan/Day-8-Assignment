e9d96bd2515207c1de0f94eaf3a3f26a8d622dbe
Chetan-charan <84982604+Chetan-charan@users.noreply.github.com>
2021-10-27 21:50:41 +0530

    Update Movies.js

--- Day 8 Assignment/Movies.js
+++ Day 8 Assignment/Movies.js
@@ -1,5 +1,5 @@
 class Movie {
-    constructor(title,studio,rating="PG") {
+    constructor(title,studio,rating="PG") {                                       //constrcutor to initialise values
         this.title = title;
         this.studio = studio;
         this.rating = rating;
@@ -14,18 +14,18 @@ class Movie {
             arr.push(object);
         }
     }
-    console.log(arr);
+    console.log(arr);                                                       //to display movies with rating PG
 
 }
 }
 
-const movie1 = new Movie("Casino Royale","Eon Productions","PG13");
+const movie1 = new Movie("Casino Royale","Eon Productions","PG13");        //creating movie objects
 const movie2 = new Movie("Spider Man","Marvel","PG");
 const movie3 = new Movie("Batman","DC","PG");
 const movie4 = new Movie("Wonder woman","DC","R")
-const movies = [movie1,movie2,movie3,movie4];
+const movies = [movie1,movie2,movie3,movie4];                               //create array of all the movies
 
-movie1.getPG(movies);
+movie1.getPG(movies);                                                      
 
 
 
