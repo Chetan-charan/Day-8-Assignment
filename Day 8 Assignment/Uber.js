bf12bbabd7b0ccf538352252e61cadf09c0490d5
Chetan-charan <84982604+Chetan-charan@users.noreply.github.com>
2021-10-27 21:46:00 +0530

    Update Uber.js

--- Day 8 Assignment/Uber.js
+++ Day 8 Assignment/Uber.js
@@ -1,13 +1,13 @@
 class Uber {
-    constructor(fare,waiting_charge){
+    constructor(fare,waiting_charge){                   //initiliase the uber fare for the passenger
         this.fare = fare;
         this.waiting_charge = waiting_charge
     }
     getPrice(){
-        return this.fare + this.waiting_charge
+        return this.fare + this.waiting_charge          // return the total payable amount of the passenger
     }
 }
 
-const passenger1 = new Uber(230,15);
+const passenger1 = new Uber(230,15);                    //passing the fare amount of the passenger
 
-console.log("Your fare for Travelling is "+passenger1.getPrice()+ " Rs");
+console.log("Your fare for Travelling is "+passenger1.getPrice()+ " Rs");    //Display the user total amount payable
